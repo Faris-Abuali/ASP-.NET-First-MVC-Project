53e0528e5bb8dcbcf8c48e2c0113427a98deb67d
Faris Abuali <fabuali@restaurant365.com>
2022-11-27 01:35:05 +0200

    First commit

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return "This is the Index Page of HelloWorldController.cs";
+            DogViewModel doggo = new DogViewModel() { Name = "Sif", Age = 2 };
+            return View(doggo);
+        }
+
+        public IActionResult Create()
+        {
+            //return View("Create");
+            return View("Create");
+        }
+
+        public string Hello()
+        {
+            return "Who's there";
+        }
+    }
+}
